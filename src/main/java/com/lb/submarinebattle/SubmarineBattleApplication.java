9cc276ad24b48435b42a54153acfb21ef91a711f
Lasha Less <lashawilldata@gmail.com>
2023-08-25 13:08:12 +0200

    initial project setup

---
+++ src/main/java/com/lb/submarinebattle/SubmarineBattleApplication.java
@@ -0,0 +1,13 @@
+package com.lb.submarinebattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmarineBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmarineBattleApplication.class, args);
+	}
+
+}
